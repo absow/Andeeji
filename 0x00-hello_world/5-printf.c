4ab54ab17fb5c5d9713edff3d01a8493b1d776cf
Absow <abdullahibashirsow@gmail.com>
2022-06-03 04:05:59 -0700

    print function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main	- main block
+ * return:0 if well executed
+ */
+int main(void) /*this is the main block */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
