85570f18681777a414fc024ea6d8d2e0fde88092
Absow <abdullahibashirsow@gmail.com>
2022-06-03 21:43:09 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print number z,
+ * followed by line
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 40; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
