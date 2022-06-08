00c5d11ec94b70f94c4ffb774d371fa117dc481f
Absow <abdullahibashirsow@gmail.com>
2022-06-08 04:47:01 -0700

    my first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the _putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char text[9] = "putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(text[i]);
+	}
+	putchar('\n');
+	
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# Description of function and nested loops
