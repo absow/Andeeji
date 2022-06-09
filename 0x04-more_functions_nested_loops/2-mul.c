f7e2292b41e29942ec7be9a022b0a17d23a8fc4d
Absow <abdullahibashirsow@gmail.com>
2022-06-09 07:30:27 -0700

    mul commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - muptiplication
+ * @a: interger
+ * @b: interger
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
