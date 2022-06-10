1315788cf69a0bf070c16ece1a68692263b779ad
Absow <abdullahibashirsow@gmail.com>
2022-06-09 20:47:13 -0700

    first commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test that print if integer is pos or neg
+ * Return: o
+ */
+int main(void)
+{
+	int 1;
+	
+	1 = 0;
+	positive_or_negative(i);
+	
+	return (0);
+}
