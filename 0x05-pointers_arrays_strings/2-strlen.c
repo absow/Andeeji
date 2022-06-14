3a2fdfbaeb905fe00719ddc86798e251a433b2d6
Absow <abdullahibashirsow@gmail.com>
2022-06-14 09:57:54 -0700

    second commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - print the length of a string
+ * @s: the string
+ * Return: 0 Always
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; len[s] != '\0'; ++len);
+	return (len);
+}
