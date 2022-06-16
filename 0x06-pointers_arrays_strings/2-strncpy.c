d2c09b9e50a9dc9613d6ab145470b82c00eac7ba
Absow <abdullahibashirsow@gmail.com>
2022-06-16 08:07:39 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,8 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		desk[i] = src[i];
+		dest[i] = src[i];
 	for ( ; i < n; i++)
-		desk[i] = '\0';
+		dest[i] = '\0';
 
 	return (dest);
