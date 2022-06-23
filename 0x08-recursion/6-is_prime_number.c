35c13ce93305ce941405ce734b6d12bcdf2d8bed
Absow <abdullahibashirsow@gmail.com>
2022-06-23 04:11:05 -0700

    first commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,5 +7,33 @@
 
 int is_prime_number(int n)
 {
-	
+	 prime(n);
+	 event(n);
+		
+}
+
+void prime(int n)
+{
+	if (n % 2 != 0)
+	{
+		_putchar("%d\n", 1);
+		return;
+	}
+	else
+	{
+		even();
+	}
+}
+
+void even(int 0)
+{
+	if (n % 2 == 0)
+	{
+		_putchar("%d\n", 0);
+		return;
+	}
+	else
+	{
+		prime();
+	}
 }
