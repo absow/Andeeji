815afbc63306e01e3fb9f99aa57c467b7046a0ea
Absow <abdullahibashirsow@gmail.com>
2022-06-27 08:38:46 -0700

    Print argc value

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - creating a function with two augmnets
+ * @argc: augument count
+ * @argv: augument vector
+ * Return: 0 (Success)
+ */
+
+int main(int argc, char *argv)
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
