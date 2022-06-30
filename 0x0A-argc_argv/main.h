9c04b3831ec336c64e24ade1f1860202aa0753f7
Absow <abdullahibashirsow@gmail.com>
2022-06-29 18:39:05 -0700

    commit

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,3 +2,5 @@ int main(int argc __attribute__((unused)), char *arg[]);
 int main(int argc, char **argv __attribute__((unused)));
 int main(int argc, char **argv __attribute__((unused)));
 int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
+
