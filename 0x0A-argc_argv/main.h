52dbfeed9ac7258c8f64b74642761c3e0a640c7e
Absow <abdullahibashirsow@gmail.com>
2022-06-29 18:29:51 -0700

    adding

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,3 +1,4 @@
 int main(int argc __attribute__((unused)), char *arg[]);
 int main(int argc, char **argv __attribute__((unused)));
 int main(int argc, char **argv __attribute__((unused)));
+int main(int argc, char *argv[]);
