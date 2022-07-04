163d3f05aca65cec65c8ad3f8d4d42567ba5bd3e
absow <abdullahibashirsow@gmail.com>
2022-07-04 11:52:23 -0700

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 
 /**
- * main - print the main file 
+ * main - print the main file
  * Return: 0 (Success)
  */
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
