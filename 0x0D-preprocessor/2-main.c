75ae835eb3820ab1324e7e080c7d0c4b30edd386
absow <abdullahibashirsow@gmail.com>
2022-07-04 09:44:42 -0700

    Printing the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the main file 
+ * Return: 0 (Success)
+ */
+int main()
+{
+	printf("The name of the file is: %s/n", __FILE__);
+	Return (0);
+}
