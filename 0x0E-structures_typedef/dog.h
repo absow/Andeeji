01516cbf2e7f7995cc87c1430c691ff247f5ef40
absow <abdullahibashirsow@gmail.com>
2022-07-05 09:06:40 -0700

    Creating header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Creating a header file and assign elements in it
+ * @name: name of the dog
+ * @age: the of the dog
+ * @owner: the name of the owner
+ */
+
 struct dog
 {
 	char *name;
